39245f8ce0742b6ec9aad639fff0049bdfbd4b90
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2021-11-20 01:54:59 +0600

    Generics, Abstract, Interface

---
+++ HW/Generics/Generics.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.*;
+class Printer{
+    public <auto> void printArray(auto[] a) {
+		for (auto i : a){
+			System.out.println(i);
+		}
+	}
+}
+
+public class Generics {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+            if(name.equals("printArray"))
+                count++;
+        }
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+    }
+}
